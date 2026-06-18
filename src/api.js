ff53f55e97cb4a43e8e5923ee28014f306a57181
djadbtlr21-droid <djadbtlr21@gmail.com>
2026-06-18 09:01:17 +0800

    Revert "fix: API_BASE 환경변수로 EdgeOne/Vercel 경로 분기"
    This reverts commit 4ad029f7554f09d189eb19bab30f10a802d325f6.

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE ?? '';
+const API_BASE = (import.meta.env.VITE_API_BASE ?? '').replace(/\/+$/, '');
 
 async function jfetch(url, opts) {
   const res = await fetch(url, opts);
